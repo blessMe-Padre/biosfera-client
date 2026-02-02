22f0a6e97dedfd45de1fe28e3412a207fe691eeb
Сергей Субботин <zarodiny@yandex.ru>
2026-02-02 16:00:16 +1000

    feat: добавляет функциональность поиска в компонент Header

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,14 +1,47 @@
 "use client";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import styles from "./style.module.scss";
 import Image from "next/image";
-import { VdsButton, VdsPanel } from "@/app/components";
+import { VdsButton, VdsPanel, Search } from "@/app/components";
 
 export default function Header() {
   const [panel, setPanel] = useState(false);
   const [panelBtn, setPanelBtn] = useState(true);
+  const [searchOpened, setSearchOpened] = useState(false);
+
+
+  // закрываем поиск при клике вне попапа
+useEffect(() => {
+  const handleClickOutside = (event: MouseEvent) => {
+      if (!searchOpened) return;
+
+      // const isClickOutsideMenu = !menuRef.current || !menuRef.current.contains(event.target as Node);
+      const isClickOutsideButton = !buttonRef.current || !buttonRef.current.contains(event.target as Node);
+
+      // if (isClickOutsideMenu && isClickOutsideButton) {
+      //     setSearchOpened(false);
+      // }
+      if (isClickOutsideButton) {
+          setSearchOpened(false);
+      }
+  };
+
+  const handleSliderClick = () => {
+      setSearchOpened(false);
+  };
+
+  document.addEventListener("mousedown", handleClickOutside);
+  document.addEventListener("sliderClick", handleSliderClick);
+  return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("sliderClick", handleSliderClick);
+  };
+}, [searchOpened]);
+
+const buttonRef = useRef<HTMLButtonElement>(null);
+
   return (
-    <header>
+    <header className="relative">
       <div className="container">
         {panel && <VdsPanel setPanel={setPanel} setPanelBtn={setPanelBtn} />}
 
@@ -27,19 +60,27 @@ export default function Header() {
               <span>+7 (924) 338-81-89</span>
             </a>
 
-            <button className={styles.header__button}>Записаться на прием</button>
+            <button className={styles.primary_button}>Записаться на прием</button>
 
             <div className={styles.header_social}>
               <button className={styles.item__button} title="Личный кабинет">
                 <Image src="/icons/account-icon.svg" className='dsv-image' alt="logo" width={22} height={22} />
               </button>
+
               {panelBtn && <VdsButton setPanel={setPanel} setPanelBtn={setPanelBtn} />}
-              <button className={styles.item__button} title="Поиск">
+
+              <button 
+                className={styles.item__button} 
+                ref={buttonRef}
+                onClick={() => setSearchOpened(!searchOpened)}
+                title="Поиск"
+                >
                 <Image src="/icons/search-icon.svg" className='dsv-image' alt="logo" width={22} height={22} />
               </button>
             </div>
         </div>
 
+        {searchOpened && <Search />}
       </div>
     </header>
   );
--- src/app/components/index.tsx
+++ src/app/components/index.tsx
@@ -2,3 +2,4 @@ export { default as Header } from "./Header/Header";
 export { default as VdsButton } from "./Vds/VdsButton";
 export { default as VdsPanel } from "./VdsPanel/VdsPanel";
 export { default as AnimateElement } from "./AnimateElement/AnimateElement";
+export { default as Search } from "./Search/Search";
