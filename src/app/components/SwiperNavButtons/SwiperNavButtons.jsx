65b76fa4d203bb665028950b88dbf439c95b2611
Сергей Субботин <zarodiny@yandex.ru>
2026-02-04 16:17:57 +1000

    feat: добавляет компонент Gallery

---
+++ src/app/components/SwiperNavButtons/SwiperNavButtons.jsx
@@ -0,0 +1,27 @@
+"use client";
+import { useSwiper } from "swiper/react";
+import styles from './style.module.css'
+
+const SwiperNavButtons = ({ addClass }) => {
+    const swiper = useSwiper();
+
+    return (
+        <div className={`${styles.swiper_nav_btns} ${addClass} ${addClass ? styles[addClass] : ''
+            }`}>
+            <button className={styles.btn_prev} onClick={() => swiper.slidePrev()}>
+                <svg width="23" height="21" viewBox="0 0 23 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M21.2491 10.7807L1.41402 11.0547M1.41402 11.0547L11.1946 1.00001M1.41402 11.0547L10.5944 19.9848" stroke="white" stroke-width="2" stroke-linecap="round" />
+                </svg>
+            </button>
+
+            <button className={styles.btn_next} onClick={() => swiper.slideNext()}>
+                <svg width="23" height="21" viewBox="0 0 23 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0.999915 10.7807L20.835 11.0547M20.835 11.0547L11.0545 1.00001M20.835 11.0547L11.6547 19.9848" stroke="white" stroke-width="2" stroke-linecap="round" />
+                </svg>
+            </button>
+        </div>
+    )
+}
+
+export default SwiperNavButtons;
+
--- src/app/sections/index.tsx
+++ src/app/sections/index.tsx
@@ -1,2 +1,3 @@
 export { default as Hero } from "./Hero/Hero";
 export { default as Services } from "./Services/Services";
+export { default as Gallery } from "./Gallery/Gallery";
