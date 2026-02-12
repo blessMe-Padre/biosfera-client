4a885cd3169e0861291272c306c3640d999f11b4
Сергей Субботин <zarodiny@yandex.ru>
2026-02-12 10:26:38 +1000

    feat: replace Nebo styles with Cookies component in Footer; update favicon

--- src/app/components/index.tsx
+++ src/app/components/index.tsx
@@ -12,3 +12,4 @@ export { default as NewsCard } from "./NewsCard/NewsCard";
 export { default as HeaderMenu } from "./HeaderMenu/HeaderMenu";
 export { default as HeaderMobileMenu } from "./HeaderMobileMenu/HeaderMobileMenu";
 export { default as ContentRenderer } from "./ContentRenderer/ContentRenderer";
+export { default as Cookies } from "./Cookies/Cookies";
