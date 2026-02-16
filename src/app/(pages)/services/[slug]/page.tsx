a84f60ad7ff0d9eb42982e758eef50a3934f1e93
Сергей Субботин <zarodiny@yandex.ru>
2026-02-16 12:25:36 +1000

    feat: implement dynamic metadata generation for services page based on slug

--- src/app/(pages)/services/[slug]/page.tsx
+++ src/app/(pages)/services/[slug]/page.tsx
@@ -2,6 +2,33 @@ import { Breadcrumbs } from "@/app/components";
 import fetchData from "@/app/utils/fetchData";
 import ContentPage from "./ContentPage";
 
+type ServicesMetadata = {
+  data: {
+    hero_title: string;
+    hero_description: string;
+    hero_image: string;
+  }[];
+};
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  const page = (await fetchData(
+    `/api/shablon-uslugis?filters[slug][$eq]=${encodeURIComponent(slug)}`,
+  )) as ServicesMetadata;
+  return {
+    title: `Биосфера ДВ - ${page?.data?.[0]?.hero_title}`,
+    description: "Услуги",
+    openGraph: {
+      title: "Услуги",
+      description: "Услуги",
+    },
+  };
+}
+
 export default async function ServicesPage({
   params,
 }: {
