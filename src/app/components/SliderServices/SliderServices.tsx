b87cc5db2acea3af39bd7de760f024eedb7e7d46
Сергей Субботин <zarodiny@yandex.ru>
2026-02-26 13:39:06 +1000

    feat: добавляет проверку на количество слайдов с услугах

--- src/app/components/SliderServices/SliderServices.tsx
+++ src/app/components/SliderServices/SliderServices.tsx
@@ -10,48 +10,50 @@ import Link from "next/link";
 import { AnimateElement, ContentRenderer } from "@/app/components";
 import type { SliderItemType } from "@/app/types";
 
-
 import "swiper/css";
 import "swiper/css/navigation";
 
 export default function SliderServices({ items }: { items: SliderItemType[] }) {
+  console.log(items);
 
   return (
     <div className={`${styles.wrapper}`}>
       <Swiper>
-        {items && items.map((slide) => (
-          <SwiperSlide key={slide.id}>
-            <div className={styles.slider__item}>
-              <div className="container">
-                <div className={styles.item_wrapper}>
-                  <div className={styles.item_content}>
-                    <ContentRenderer content={slide?.content ?? []} />
-                    <AnimateElement animationDelay={100}>
-                      <Link href={slide?.link ?? ""} className={styles.item__link}>
-                        Узнать подробнее
-                      </Link>
-                    </AnimateElement>
-                  </div>
+        {items &&
+          items.map((slide) => (
+            <SwiperSlide key={slide.id}>
+              <div className={styles.slider__item}>
+                <div className="container">
+                  <div className={styles.item_wrapper}>
+                    <div className={styles.item_content}>
+                      <ContentRenderer content={slide?.content ?? []} />
+                      <AnimateElement animationDelay={100}>
+                        <Link
+                          href={slide?.link ?? ""}
+                          className={styles.item__link}
+                        >
+                          Узнать подробнее
+                        </Link>
+                      </AnimateElement>
+                    </div>
 
-                  <div className={styles.slider__item_image}>
-                    <Image
-                      src={`${process.env.NEXT_PUBLIC_API_SERVER}${slide?.image?.[0]?.url}`}
-                      alt="slider-1"
-                      width={405}
-                      height={478}
-
-                      className="dsv-image"
-                      placeholder="blur"
-                      blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTQ0MiIgaGVpZ2h0PSIxMTg5IiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9IiNjY2MiIC8+PC9zdmc+"
-
-                    />
+                    <div className={styles.slider__item_image}>
+                      <Image
+                        src={`${process.env.NEXT_PUBLIC_API_SERVER}${slide?.image?.[0]?.url}`}
+                        alt="slider-1"
+                        width={405}
+                        height={478}
+                        className="dsv-image"
+                        placeholder="blur"
+                        blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTQ0MiIgaGVpZ2h0PSIxMTg5IiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9IiNjY2MiIC8+PC9zdmc+"
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </SwiperSlide>
-        ))}
-        {items && items.length > 0 && (
+            </SwiperSlide>
+          ))}
+        {items && items.length > 1 && (
           <SwiperNavButtons addClass={"buttons_bottom"} />
         )}
       </Swiper>
