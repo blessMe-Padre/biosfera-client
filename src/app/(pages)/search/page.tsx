37c6ceff49d69cf579276821346748d9ea19c407
Сергей Субботин <zarodiny@yandex.ru>
2026-03-05 10:06:35 +1000

    feat: обновляет компонент поиска, добавляет поддержку управления состоянием открытия и улучшает стили для элементов списка

---
+++ src/app/(pages)/search/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+
+function SearchResults() {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query");
+
+  return (
+    <Suspense>
+      <div className="container">
+        <h1>Результаты поиска для: {query}</h1>
+      </div>
+    </Suspense>
+  );
+}
+
+export default function SearchPage() {
+  return (
+    <Suspense
+      fallback={
+        <div className="container">
+          <h1>Загрузка результатов поиска...</h1>
+        </div>
+      }
+    >
+      <SearchResults />
+    </Suspense>
+  );
+}
