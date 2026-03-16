8abc235a75e756f28651b4138da2d852de82fe37
Сергей Субботин <zarodiny@yandex.ru>
2026-03-16 12:22:24 +1000

    feat: заменяет список промо-элементов на новый компонент ContentPage

--- src/app/(pages)/promo/page.tsx
+++ src/app/(pages)/promo/page.tsx
@@ -1,6 +1,5 @@
 import Breadcrumbs from "@/app/components/Breadcrumbs/Breadcrumbs";
 
-import styles from "./style.module.scss";
 import fetchData from "@/app/utils/fetchData";
 import { notFound } from "next/navigation";
 import Image from "next/image";
@@ -8,6 +7,8 @@ import ContentRenderer, {
   type ContentItem,
 } from "@/app/components/ContentRenderer/ContentRenderer";
 import PromoButton from "./button";
+import ContentPage from "./ContentPage";
+import styles from "./style.module.scss";
 
 type PromoItem = {
   id: number;
@@ -52,33 +53,7 @@ export default async function Promo() {
         качественная помощь была доступнее и удобнее для наших пациентов.
       </p>
 
-      <ul className={styles.promo_list}>
-        {page.data.map((item) => (
-          <li key={item.id} className={styles.promo_item}>
-            <div className={styles.promo_item_content}>
-              <div className={styles.promo_image}>
-                <Image
-                  className="dsv-image"
-                  src={
-                    item.image?.url
-                      ? `${domain}${item.image.url}`
-                      : "/placeholder1.svg"
-                  }
-                  alt="News"
-                  width={288}
-                  height={180}
-                />
-              </div>
-              <h3 className={`${styles.promo_item_title} text-gradient`}>
-                {item.title}
-              </h3>
-              <ContentRenderer content={item.description ?? []} />
-            </div>
-
-            <PromoButton />
-          </li>
-        ))}
-      </ul>
+      <ContentPage data={page.data} />
     </div>
   );
 }
